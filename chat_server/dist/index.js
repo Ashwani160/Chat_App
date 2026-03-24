cb937872778d61220f36ffe5811fb8e20d598642
Ashwani160 <ashwani11002004@gmail.com>
2026-03-24 16:49:39 +0530

    push basic server and client

---
+++ chat_server/dist/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { WebSocketServer, WebSocket } from "ws";
+const app = express();
+app.use(express.json());
+// HTTP route
+app.get("/", (_req, res) => {
+    res.send("HTTP is working");
+});
+// Start Express (returns HTTP server)
+const server = app.listen(8080, () => {
+    console.log("HTTP server running on http://localhost:8080");
+});
+// Attach WebSocket to Express server
+const wss = new WebSocketServer({ server });
+let userCount = 0;
+wss.on('connection', function connection(ws) {
+    userCount++;
+    console.log(`User Count: ${userCount}`);
+    ws.on('error', console.error);
+    ws.on('message', function message(data, isBinary) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        });
+    });
+    ws.send('something');
+});
+//# sourceMappingURL=index.js.map
